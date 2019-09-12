5363bdfc35062b937d2577702a90eaa21aa1abca
Calvin-Davidson <53999981+Calvin-Davidson@users.noreply.github.com>
2019-09-12 12:01:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("#############################################################################################");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-----Welkom bij - STEEN PAPIER SCHAAR-----=-=-=-=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-----Klaar om het geweldige avondtuur te beginnen?-----=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("#############################################################################################");
+        
+        }
+        public int RandomNumber(int min, int max)  
+{  
+    Random random = new Random();  
+    return random.Next(min, max);  
+}  
+    }
+}
